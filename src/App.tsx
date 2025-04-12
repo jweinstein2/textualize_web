e0657bf4fdf1810ec20bc41f15df39ff057281ac
jweinstein2 <jaredweinstein1@gmail.com>
2025-04-11 21:30:31 -0700

    Add posthog logging

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import './App.css'
 import Hero from "./components/Hero"
 import Privacy from './components/Privacy'
 import Footer from './components/Footer'
-import DownloadButton from './components/DownloadButton'
+
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+posthog.init('phc_R24eFyS4g39gx6F3GrmxNb6HqeGX0dTWx7gfmWUNx2Q', {
+  api_host: 'https://us.i.posthog.com',
+})
 
 function App() {
   return (
+    <PostHogProvider client={posthog}>
     <div className="container">
       <Hero />
       <Privacy />
       <Footer />
     </div>
+    </PostHogProvider>
   )
 }
 
